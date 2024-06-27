37242e7e6e351d490fd111acc36a843022c0b133
Ibrar Aansari <mdibrar1070@gmail.com>
2024-06-27 11:13:51 +0530

    Add files via upload

---
+++ NavBar1.js
@@ -0,0 +1,15 @@
+import React from "react";
+function NavBar1() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand " href="/">
+            Infosphere
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+export default NavBar1;
